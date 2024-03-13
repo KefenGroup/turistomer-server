20c226a84e04a16b8f6670817c014ffaf8ddcc06
oguzkaan <oguzkaanoselmis.ok@gmail.com>
2024-03-13 18:07:35 +0300

    Model script updated

--- src/main/resources/scripts/model/TuristOmerNERModel.py
+++ src/main/resources/scripts/model/TuristOmerNERModel.py
@@ -1,11 +1,8 @@
 from tner import TransformersNER
 from fastapi import FastAPI
-from typing import List, Tuple, Dict, Any
+from typing import List
 from pydantic import BaseModel
-
-model_name = "tner/roberta-large-mit-restaurant"
-model = TransformersNER(model_name)
-app = FastAPI()
+import uvicorn
 
 class PromptInput(BaseModel):
     prompt: str
@@ -16,8 +13,16 @@ class EntityPrediction(BaseModel):
     position: List[int]
     probability: List[float]
 
+
+model_name = "tner/roberta-large-mit-restaurant"
+model = TransformersNER(model_name)
+app = FastAPI()
+
 @app.post("/predict/", response_model=List[EntityPrediction])
 async def predict(input_prompt: PromptInput):
     text = input_prompt.prompt
     prediction = model.predict([text])
     return prediction['entity_prediction'][0]
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
